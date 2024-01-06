04c35425e5b4b7d9aee9ad1da2be7aa6d648da52
Srinivas <srinu781997@gmail.com>
2024-01-06 16:08:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function addProject(projectName, projectDescription){
+    let template = `<div class="project-box shadow">
+    <div class="project-text">
+        <div class="project-content p-2">
+            <p class="projects-title">${projectName}</p>
+            <p class="projects-content">${projectDescription}</p>
+            <button class="view-project-button btn btn-default">View Project</button>
+        </div>
+    </div>
+    
+    <div class="project-image"><img src="images/project2.png"></div>
+    </div>`
+    let projectsNode = document.getElementById("projectsSection");
+    projectsNode.innerHTML = template + projectsNode.innerHTML;
+}
+
+function clearForm() {
+    let projectName = document.getElementById("project-name");
+    let projectDescription = document.getElementById("project-description");
+    let projectLink = document.getElementById("project-link");
+    projectName.value="";
+    projectDescription.value="";
+    if(projectLink){
+        projectLink.value="";
+    }
+
+}
+
+let addProjectAction = document.getElementById("addProjectForm");
+addProjectAction.onsubmit = function(event) {
+    event.preventDefault();
+    let projectName = document.getElementById("project-name");
+    let projectDescription = document.getElementById("project-description");
+    addProject(projectName?.value, projectDescription?.value);
+    clearForm();
+    document.getElementById("navItem2").click();
+}
+
+
+
